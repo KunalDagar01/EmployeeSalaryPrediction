3091e3a512e6687e0c1420f13b4dd1d465035b47
KunalDagar01 <kunaldagar038@gmail.com>
2023-10-14 22:52:26 +0530

    fouth one

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ try:
             
             
     if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=8080)
+        app.run()
 except Exception as e:
     logging.info(CustomException(e,sys))
     raise CustomException(e,sys)
